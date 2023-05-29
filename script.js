a388700ae0a1f6459119a50658bac30ab30d407f
JesusTheGoat7 <116693360+JesusTheGoat7@users.noreply.github.com>
2023-05-29 17:39:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+const input = document.querySelector("#fruit");
+const suggestions = document.querySelector(".suggestions ul");
+
+const fruit = [
+  "Apple",
+  "Apricot",
+  "Avocado 🥑",
+  "Banana",
+  "Bilberry",
+  "Blackberry",
+  "Blackcurrant",
+  "Blueberry",
+  "Boysenberry",
+  "Currant",
+  "Cherry",
+  "Coconut",
+  "Cranberry",
+  "Cucumber",
+  "Custard apple",
+  "Damson",
+  "Date",
+  "Dragonfruit",
+  "Durian",
+  "Elderberry",
+  "Feijoa",
+  "Fig",
+  "Gooseberry",
+  "Grape",
+  "Raisin",
+  "Grapefruit",
+  "Guava",
+  "Honeyberry",
+  "Huckleberry",
+  "Jabuticaba",
+  "Jackfruit",
+  "Jambul",
+  "Juniper berry",
+  "Kiwifruit",
+  "Kumquat",
+  "Lemon",
+  "Lime",
+  "Loquat",
+  "Longan",
+  "Lychee",
+  "Mango",
+  "Mangosteen",
+  "Marionberry",
+  "Melon",
+  "Cantaloupe",
+  "Honeydew",
+  "Watermelon",
+  "Miracle fruit",
+  "Mulberry",
+  "Nectarine",
+  "Nance",
+  "Olive",
+  "Orange",
+  "Clementine",
+  "Mandarine",
+  "Tangerine",
+  "Papaya",
+  "Passionfruit",
+  "Peach",
+  "Pear",
+  "Persimmon",
+  "Plantain",
+  "Plum",
+  "Pineapple",
+  "Pomegranate",
+  "Pomelo",
+  "Quince",
+  "Raspberry",
+  "Salmonberry",
+  "Rambutan",
+  "Redcurrant",
+  "Salak",
+  "Satsuma",
+  "Soursop",
+  "Star fruit",
+  "Strawberry",
+  "Tamarillo",
+  "Tamarind",
+  "Yuzu",
+];
+
+function search(str) {
+  let results = fruit.filter(function (f) {
+    return f.toLowerCase().includes(str.toLowerCase());
+  });
+
+  return results;
+}
+
+function searchHandler(e) {
+  const inputVal = e.target.value;
+  const results = search(inputVal);
+  showSuggestions(results, inputVal);
+}
+
+function showSuggestions(results, inputVal) {
+  suggestions.innerHTML = "";
+  if (results.length > 0) {
+    suggestions.classList.add("has-suggestions");
+
+    for (let i = 0; i < results.length; i++) {
+      const li = document.createElement("li");
+      li.textContent = results[i];
+      suggestions.appendChild(li);
+    }
+  } else {
+    suggestions.classList.remove("has-suggestions");
+  }
+}
+
+function useSuggestion(e) {
+  const selectedSuggestion = e.target.textContent;
+  input.value = selectedSuggestion;
+  suggestions.innerHTML = "";
+}
+
+input.addEventListener("keyup", searchHandler);
+suggestions.addEventListener("click", useSuggestion);
